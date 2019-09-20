6ee64bcd17c9c41c3ffc916ec9d2004bf4369850
Dasha Sergeeva <dashasergeeva101@gmail.com>
2019-09-20 23:18:17 +0300

    init commit

--- random_letter1(с проверкой).py
+++ random_letter1(с проверкой).py
@@ -6,10 +6,13 @@ if r==len(s): d=s.pop(r-1)
 else: d=s.pop(r)
 print('Само слово с эти индексом:',d) 
 
-bukva =random.randint(0,len(d))
-print('Индекс буквы',bukva) 
-print('буква,которую загадали:',d[bukva-1]) 
-print (d[+0:(bukva-1)]+"?"+d[+(bukva):])
+'''bukva =random.randint(0,len(d))'''
+bukva=0
+print('Индекс буквы',bukva)
+if bukva==0: m=bukva+1
+else: m=bukva;
+print('буква,которую загадали:',d[m-1]) 
+print (d[+0:m-1]+"?"+d[+m:])
 x=str(input('Введите вашу букву:'))
-if x==d[bukva-1]: print ('Вы угадали букву!!')
+if x==d[m-1]: print ('Вы угадали букву!!')
 else: print('В следующий раз получится!')
--- random_letter1.py
+++ random_letter1.py
@@ -3,9 +3,10 @@ s=['пирамида','треугольник','овал']
 r=random.randint(0,len(s))                
 if r==len(s): d=s.pop(r-1)
 else: d=s.pop(r)                  
-bukva =random.randint(0,len(d))           
-                                          
-print (d[+0:(bukva-1)]+"?"+d[+(bukva):])
+bukva =random.randint(0,len(d))
+if bukva==0: m=bukva+1
+else: m=bukva;
+print (d[+0:(m-1)]+"?"+d[+m:])
 x=str(input('Введите вашу букву:'))
 if x==d[bukva-1]: print ('Вы угадали букву!!')
 else: print('В следующий раз получится!')
